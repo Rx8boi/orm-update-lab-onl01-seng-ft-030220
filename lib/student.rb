7379d2c8767c06dc8e502c2ec86cab8901c0a6ff
Charles Butler <cdbutle@outlook.com>
2020-04-12 18:31:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,10 @@ class Student
   end
 
   def save
-    sql = <<-SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
     INSERT INTO students (name, grade)
     VALUES (?, ?)
     SQL
