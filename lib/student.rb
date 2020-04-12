73cdbbe788c8504e67c398a56f1b032de40c38d9
Charles Butler <cdbutle@outlook.com>
2020-04-12 18:30:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ def self.find_by_name(name)
 end
 
 def update
-  sql  "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
   DB[:conn].execute(sql, self.name, self.grade, self.id)
 end
   # Remember, you can access your database connection anywhere in this class
